19cffcb6d198d68eef8db28a7e55452af85a2798
Divij <divij.mehra.16@gmail.com>
2017-09-08 07:29:07 +0900

    added thigns

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { EditTodoPage } from '../edit-todo/edit-todo'
 import { DataProvider } from '../../providers/data/data';
+import { s3 } from 'fine-uploader/lib/s3';
+import { FineUploader, UIOptions } from 'fine-uploader';
  
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  s3UIOptions: s3.S3UIOptions;
+  s3Uploader: s3.FineUploader;
 
   // todos: any; 
 
@@ -34,4 +39,40 @@ export class HomePage {
       todo: todo 
     })
   }
+
+  ionViewDidEnter() {
+       
+    this.s3UIOptions = {
+      debug: true,
+      element: document.getElementById('fine-uploader-manual-trigger'),
+      template: "qq-template-manual-trigger",
+      request: {
+          endpoint: 'http://fineuploader-test-attachment.s3.amazonaws.com',
+          accessKey: 'AKIAIARVO27XY6WOEDTQ'
+      },
+      signature: {
+          endpoint: '/server'
+      },
+      // uploadSuccess: {
+      //     endpoint: '/server'
+      // },
+      // iframeSupport: {
+      //     localBlankPagePath: '/server'
+      // },
+      retry: {
+          enableAuto: true // defaults to false
+      }
+      // deleteFile: {
+      //     enabled: true, // defaults to false
+      //     endpoint: '/s3handler'
+      // }
+    }
+    this.s3Uploader = new s3.FineUploader(this.s3UIOptions);
+
+  }
+
+  uploadFiles() {
+    this.s3Uploader.uploadStoredFiles();
+  }
+
 }
